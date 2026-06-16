8320ff377c3eb0380884f731bcfcfba891e2e4a4
Ap89-24 <aman082199@gmail.com>
2026-06-16 19:26:04 +0530

    basic backend setup

---
+++ Backend/src/models/message.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const messageSchema = new Schema({
+  chat: {
+    type: Schema.Types.ObjectId,
+    ref: "Chat",
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  role: {
+    type: String,
+    enum: ["user", "AI"],
+    required: true,
+    default: "user"
+  }
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+messageSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+export default model("Message", messageSchema);
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+export default model("User", userSchema);
