94d30ed6a0fa5b86bc1420bc2f415dcea8858056
Ap89-24 <aman082199@gmail.com>
2026-06-18 22:50:06 +0530

    implement a login validator

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import registerValidator from "../validators/register.validator.js";
 import { register, verifyEmail } from "../controllers/auth.controller.js";
+import loginValidator from "../validators/login.validator.js";
 
 const authRouter = Router();
 
@@ -15,9 +16,19 @@ authRouter.post("/register", registerValidator, register);
 
 
 /* 
+@description: Login an existing user
+@route: POST /api/auth/login
+@access: Public
+@note: The login route will validate the user's credentials and return a JWT token if the login is successful.
+*/
+authRouter.post("/login" , loginValidator);
+
+
+/* 
 @description: Verify the user's email address
 @route: GET /api/auth/verify-email          
 @access: Public
+@note: The verification link will contain a token that the user can click to verify their email address.
 */
 authRouter.get("/verify-email" , verifyEmail);
 
---
+++ Backend/src/validators/login.validator.js
@@ -0,0 +1,21 @@
+import { body, validationResult } from "express-validator";
+
+
+const loginValidator = [
+  body("email")
+    .trim()
+    .notEmpty().withMessage("Email is required")
+    .isEmail().withMessage("Email must be valid"),
+  body("password")
+    .notEmpty().withMessage("Password is required")
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+    },
+];
+
+export default loginValidator;
