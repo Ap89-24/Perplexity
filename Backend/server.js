92d55f383cc59aaa33084c70d247200143c8453a
Ap89-24 <aman082199@gmail.com>
2026-06-23 11:23:02 +0530

    some improvement

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
+import testAi from "./src/services/ai.service.js";
 
 
 
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
 app.listen(PORT , () => {
     console.log(`Server in running on port ${PORT}`);
 })
