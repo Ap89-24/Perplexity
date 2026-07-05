19bfd7cd47a5e9e9440fef88057b7f2fe82e6734
Ap89-24 <aman082199@gmail.com>
2026-07-05 13:15:22 +0530

    implementing chat hook layer

--- Frontend/src/features/chat/hooks/useChat.js
+++ Frontend/src/features/chat/hooks/useChat.js
@@ -1,9 +1,31 @@
 import { initSocketConnection } from "../services/chat.socket.js";
-import { sendMessage , getChats , getMessages , deleteChat } from "../services/chat.api.js";
+import { sendMessage, getChats, getMessages, deleteChat } from "../services/chat.api.js";
+import { setChats, setCurrentChatId, setIsLoading, setError } from "../chat.slice.js";
+import { useDispatch } from "react-redux";
+
 
 
 export const useChat = () => { 
+
+    const dispatch = useDispatch();
+
+    const handleSendMesage = async ({ message, chatId }) => { 
+        dispatch(setIsLoading(true));
+        const data = await sendMessage({ message, chatId });
+        const { chat, AiMessages } = data;
+        dispatch(setChats((prev) => {
+            return {
+                ...prev,
+                [chat.title]: {
+                    ...chat,
+                    message: [{ content: message, role: "user" }, AiMessages]
+                },
+            }
+        }));
+        dispatch(setCurrentChatId(chat._id));
+    };
     return {
         initSocketConnection,
+        handleSendMesage
     }
 };
